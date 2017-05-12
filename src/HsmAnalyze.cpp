114d4a25b224ba76c4c621b66030ef2815b33e04
Antonio Maiorano <amaiorano@gmail.com>
2017-05-12 17:35:08 -0400

    Add command line args -map and -dot to print state-transition map and dot file respectively

--- src/HsmAnalyze.cpp
+++ src/HsmAnalyze.cpp
@@ -1,8 +1,6 @@
-// Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
 #include "llvm/Support/CommandLine.h"
 
 #include "DotGenerator.h"
@@ -14,22 +12,34 @@ using namespace clang::ast_matchers;
 using namespace clang::tooling;
 using namespace llvm;
 
-// Apply a custom category to all command-line options so that they are the
-// only ones displayed.
-static llvm::cl::OptionCategory MyToolCategory("hsm-analyze options");
+static const char *ToolDescription = "A tool that analyzes C++ code that makes "
+                                     "use of the HSM library "
+                                     "(https://github.com/amaiorano/hsm)\n";
 
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
+static cl::OptionCategory HsmAnalyzeCategory("hsm-analyze options");
+
+static cl::opt<bool> PrintMap("map", cl::desc("Print state-transition map"),
+                              cl::cat(HsmAnalyzeCategory));
+
+static cl::opt<bool>
+    PrintDotFile("dot", cl::desc("Print state-transition dot file contents"),
+                 cl::cat(HsmAnalyzeCategory));
+
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
-// A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...");
-
 int main(int argc, const char **argv) {
-  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+  CommonOptionsParser OptionsParser(argc, argv, HsmAnalyzeCategory,
+                                    ToolDescription);
+
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
+
+  if (!(PrintMap || PrintDotFile)) {
+    llvm::errs() << "Nothing to do. Please specify "
+                    "an action to perform.\n\n";
+    cl::PrintHelpMessage();
+    return -1;
+  }
 
   StateTransitionMap Map;
   MatchFinder Finder;
@@ -40,7 +50,6 @@ int main(int argc, const char **argv) {
     return Result;
   }
 
-  bool PrintMap = false;
   if (PrintMap) {
     for (auto &kvp : Map) {
       auto SourceStateName = kvp.first;
@@ -50,10 +59,13 @@ int main(int argc, const char **argv) {
       llvm::outs() << SourceStateName << " "
                    << TransitionTypeVisualString[static_cast<int>(TransType)]
                    << " " << TargetStateName << "\n";
+      llvm::outs().flush();
     }
   }
 
-  auto DotFileContents = DotGenerator::generateDotFileContents(Map);
-  llvm::outs() << DotFileContents;
-  llvm::outs().flush();
+  if (PrintDotFile) {
+    auto DotFileContents = DotGenerator::generateDotFileContents(Map);
+    llvm::outs() << DotFileContents;
+    llvm::outs().flush();
+  }
 }
